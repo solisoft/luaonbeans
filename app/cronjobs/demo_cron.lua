8a75cfb1121b4616651145a76adf72752f8e06e7
Olivier Bonnaure <olivier@solisoft.net>
2025-05-21 11:23:02 +0200

    fix: docker compose file & democron syntax issue

--- app/cronjobs/demo_cron.lua
+++ app/cronjobs/demo_cron.lua
@@ -1,7 +1,7 @@
 return {
   action = HandleCronJob("*/3 * * * *", function()
     local t1 = GetTime()
-    Log(kLogInfo, "\n" .. UuidV4()
+    Log(kLogInfo, "\n" .. UuidV4())
     Log(kLogInfo, "\nThis runs every 3 minutes : " .. os.date("%Y-%m-%d %H:%M:%S"))
     Log(kLogInfo, "\nTime taken: " .. (GetTime() - t1))
   end)
